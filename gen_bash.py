e5b0fd7b696578bdbf9db3af63dad7765fee9ab5
Toto Minai <bufhdy@hotmail.com>
2020-07-10 17:18:52 +0800

    Upload 2019 articles

--- gen_bash.py
+++ gen_bash.py
@@ -1,34 +1,41 @@
 import requests, os
 from bs4 import BeautifulSoup
 
-r = requests.get('https://cut-and-go.org')
+r = requests.get('https://totstories.typlog.io')
 soup = BeautifulSoup(r.text, 'html.parser')
 for div in soup.find_all(attrs={"class": "item"}):
     href = div.find(attrs={"class": "item-main"})['href'][1:]
     title = div.h3.string
     filename = href.split('/')[1]
 
-#     adr = div['href'][1:]
-    new = href+'/index.html'
+    r_deep = requests.get('https://totstories.typlog.io/'+href)
+    soup_deep = BeautifulSoup(r_deep.text, 'html.parser')
+    text = soup_deep.find(attrs={"class": "e-content"})
+
+#     inner
+
+    new = href+'/{}.html'.format(filename)
+    print(new)
     os.makedirs(os.path.dirname(new))
     f = open(new, 'w')
-    text = '''<!DOCTYPE html>
-<html lang="zh-tw">
-<head>
-    <meta charset="UTF-8" />
-    <link rel="icon" type="image/x-icon" href="https://cut-and-go.org/img/icon.png" />
-    <link rel="apple-touch-icon" href="https://cut-and-go.org/img/apple-touch-icon.png" />
-    <title>{}</title>
-    <meta name="description" content="{}" />
-    <meta property="og:site_name" content="Cut-and-Go" />
-    <meta property="og:title" content="Cut-and-Go" />
-    <meta property="og:description" content="{}" />
-    <meta property="og:image" content="https://cut-and-go.org/img/cover.png" />
-    <meta name="twitter:card" content="summary" />
-    <meta name="twitter:creator" content="@bufhdy" />
-    <meta property="og:url" content="https://cut-and-go.org/" />
-    <meta http-equiv="refresh" content="0; url=https://cut-and-go.org/{}/{}.pdf" />
-    </style>
-</head>
-</html>'''.format(title, title, title, href, filename)
-    f.write(text)
+    f.write(str(text))
+#     text = '''<!DOCTYPE html>
+# <html lang="zh-tw">
+# <head>
+#     <meta charset="UTF-8" />
+#     <link rel="icon" type="image/x-icon" href="https://cut-and-go.org/img/icon.png" />
+#     <link rel="apple-touch-icon" href="https://cut-and-go.org/img/apple-touch-icon.png" />
+#     <title>{}</title>
+#     <meta name="description" content="{}" />
+#     <meta property="og:site_name" content="Cut-and-Go" />
+#     <meta property="og:title" content="Cut-and-Go" />
+#     <meta property="og:description" content="{}" />
+#     <meta property="og:image" content="https://cut-and-go.org/img/cover.png" />
+#     <meta name="twitter:card" content="summary" />
+#     <meta name="twitter:creator" content="@bufhdy" />
+#     <meta property="og:url" content="https://cut-and-go.org/" />
+#     <meta http-equiv="refresh" content="0; url=https://cut-and-go.org/{}/{}.pdf" />
+#     </style>
+# </head>
+# </html>'''.format(title, title, title, href, filename)
+#     f.write(text)
