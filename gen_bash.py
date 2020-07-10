eace090c8f99c8525cdbe42283cfaca1be7775c6
Toto Minai <bufhdy@hotmail.com>
2020-07-10 14:27:35 +0800

    Correct HTML

--- gen_bash.py
+++ gen_bash.py
@@ -16,22 +16,9 @@ for div in soup.find_all(attrs={"class": "item"}):
 <html lang="zh-tw">
 <head>
     <meta charset="UTF-8" />
-    <link rel="icon" type="image/x-icon" href="https://cut-and-go.org/img/icon.png" />
-    <link rel="apple-touch-icon" href="https://cut-and-go.org/img/apple-touch-icon.png" />
     <title>{}</title>
-    <meta name="description" content="{}" />
-    <meta property="og:site_name" content="Cut-and-Go" />
-    <meta property="og:title" content="Cut-and-Go" />
-    <meta property="og:description" content="{}" />
-    <meta property="og:image" content="https://cut-and-go.org/img/cover.png" />
-    <meta name="twitter:card" content="summary" />
-    <meta name="twitter:creator" content="@bufhdy" />
-    <meta property="og:url" content="https://cut-and-go.org/" />
-    <meta http-equiv=”refresh” content=”0;url=https://cut-and-go.org/{}/{}.pdf” />
+    <meta http-equiv=”refresh” content=”0; url=https://cut-and-go.org/{}/{}.pdf” />
     </style>
 </head>
-<body>
-    <img src="to-be-continue.svg" id="text" />
-</body>
-</html>'''.format(title, title, title, href, filename)
+</html>'''.format(title, href, filename)
     f.write(text)
